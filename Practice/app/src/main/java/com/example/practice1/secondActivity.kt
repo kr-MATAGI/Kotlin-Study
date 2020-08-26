3e3bfbbd60070fbdaf87bfcf44fd7767b4272563
Jae Hoon <dev_matagi@kakao.com>
2020-08-26 12:57:02 +0900

    layout change event test

---
+++ Practice/app/src/main/java/com/example/practice1/secondActivity.kt
@@ -0,0 +1,15 @@
+package com.example.practice1
+
+import android.app.AppComponentFactory
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class SecondActivity : AppCompatActivity()
+{
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.second_layout)
+
+    }
+
+}
